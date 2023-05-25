2cf642c6f563a8b75408e3fd35a8337f74104951
Russel Jerome Rapi <111546620+syllablelous@users.noreply.github.com>
2023-05-25 12:17:17 +0800

    Create Vehicle.java

---
+++ 5-25-2023/Vehicle.java
@@ -0,0 +1,11 @@
+//Acts as the superclass for Car, Plane, and Boat
+
+public class Vehicle {
+	float speed;
+	String color;
+	int price;
+	
+	public void stop() {
+		System.out.println("Stopping vehicle...");
+	}
+}
