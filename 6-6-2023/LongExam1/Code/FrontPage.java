d3217b7ac1cab727a546d861e04b3c03d752d85a
Russel Jerome Rapi <111546620+syllablelous@users.noreply.github.com>
2023-06-06 22:57:01 +0800

    Create FrontPage.java
    Displays the front page of the portfolio.

---
+++ 6-6-2023/LongExam1/Code/FrontPage.java
@@ -0,0 +1,80 @@
+package FinalGUI; //You can omit this line or edit it to match your corresponding package if you want to run the code.
+
+import java.awt.EventQueue;
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.io.*;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrontPage extends JFrame{
+
+	private JPanel contentPane;
+
+	// Launch the application.
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrontPage frame = new FrontPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	// Create the frame.
+	 
+	public FrontPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 80, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel backgroundLabel = new JLabel("");
+		backgroundLabel.setBounds(5, 5, 1280, 720);
+		
+		try {
+			// You can edit the file path inside new File() in order to match the current file path of the image file.
+			ImageIcon img = new ImageIcon(ImageIO.read(new File("/Users/russeljeromerapi/Downloads/FrontPage.png")).getScaledInstance(1280, 720, Image.SCALE_SMOOTH));
+			backgroundLabel.setIcon(img); //Allows for me to set the background for the JFrame window using the image from ImageIcon
+		}
+		catch (IOException e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+		contentPane.setLayout(null);
+		contentPane.add(backgroundLabel);
+		
+		JButton proceedButton = new JButton(""); //Proceed Button
+		proceedButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// When this button is clicked, it will simply close this window and open up the PortfolioPages window.
+				dispose();
+				PortfolioPages pages = new PortfolioPages();
+				pages.setVisible(true);
+			}
+		});
+		
+		proceedButton.setBounds(532, 499, 226, 111);
+		// The following three lines of code allow for my button to appear invisible while still being able to be clicked on.
+		proceedButton.setOpaque(false);
+		proceedButton.setContentAreaFilled(false);
+		proceedButton.setBorderPainted(false);
+		contentPane.add(proceedButton);
+	}
+}
